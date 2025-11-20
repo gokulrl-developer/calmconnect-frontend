149f13d2fe95f5db709141c7ee12a9fc551b4f6c
Gokul R.L <gokulrlsreepath95@gmail.com>
2025-11-20 17:27:55 +0530

    solved typescript errors on frontend

--- src/components/ErrorWatcher.tsx
+++ src/components/ErrorWatcher.tsx
@@ -1,12 +1,12 @@
 // Example in a component
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import type { RootState } from "../store";
+import type { IRootState } from "../store";
 import { clearError } from "../features/statusSlice";
 
 export default function ErrorWatcher() {
   const dispatch = useDispatch();
-  const {success,error} = useSelector((state: RootState) => state.status);
+  const {success,error} = useSelector((state: IRootState) => state.status);
 
   useEffect(() => {
     if (error) {
